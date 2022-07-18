88948019b50656d36bb830f39941b940b4bee5c2
Chernyackoff <chernyackoff.vlad@yandex.ru>
2022-07-18 23:05:26 +0300

    AutoRu parser without pic urls

--- src/parsers/adapter.py
+++ src/parsers/adapter.py
@@ -1,6 +1,32 @@
-from typing import TypedDict
+from typing import TypedDict, NamedTuple
+from webdriver_manager.firefox import GeckoDriverManager
+from selenium.webdriver.firefox.service import Service
+from selenium.webdriver.firefox.options import Options
+from selenium import webdriver
 
 
 class Car(TypedDict):
     brand: str
     model: str
+
+
+class CarCard(TypedDict):
+    url: str
+    image_src: str
+    name: str
+    milage: str
+    color: str
+    year: str
+    price: str
+    site: str
+
+
+class Session():
+    def __init__(self):
+        # options = Options()
+        # options.add_argument("--headless")
+        self.driver = webdriver.Firefox(service=Service(GeckoDriverManager().install()))
+        print("Driver evoked")
+
+    def close(self):
+        self.driver.close()
--- src/parsers/autoru_parser.py
+++ src/parsers/autoru_parser.py
@@ -1,8 +1,14 @@
-from adapter import Car
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.common.action_chains import ActionChains
+from src.parsers.adapter import Car, CarCard
+from src.parsers.adapter import Session
+from selenium.webdriver.support import expected_conditions as ec
+from selenium.webdriver.common.by import By
 
 
 class AutoRuParser():
-    def __init__(self, parameters: Car):
+    def __init__(self, parameters: Car, session: Session):
+        self.driver = session.driver
         self.params = parameters
         self.url = self.compose_url()
 
@@ -15,4 +21,60 @@ class AutoRuParser():
             raise ValueError("No way")
 
     def parse(self) -> list:
-        pass
+        act = ActionChains(self.driver)
+        self.driver.get(self.url)
+        result = []
+        # get all car cards
+        try:
+            cars = self.driver.find_elements(By.CLASS_NAME, "ListingItem")
+        except Exception as e:
+            print(f"Error: {e}")
+            raise ValueError("No items found")
+
+        for item in cars:
+            car = CarCard()
+            try:
+                info = item.find_element(By.CLASS_NAME, "ListingItemTitle__link")
+                name = info.text
+                url = info.get_attribute('href')
+
+                info = item.find_elements(By.CLASS_NAME, "ListingItemTechSummaryDesktop__column")
+                color = info[-1].find_elements(By.CLASS_NAME, "ListingItemTechSummaryDesktop__cell")[-1].text
+
+                price = item.find_element(By.CLASS_NAME, "ListingItemPrice__content").text
+                price = ''.join(price.split("&nbsp;"))
+
+                mileage = item.find_element(By.CLASS_NAME, "ListingItem__kmAge").text
+                mileage = ''.join(mileage.split("&nbsp;"))
+
+                year = item.find_element(By.CLASS_NAME, "ListingItem__year").text
+
+                # img = item.find_element(By.CLASS_NAME, "Brazzers__image-wrapper")
+                # img_link = img.find_element(By.TAG_NAME, "img").get_attribute('src')
+
+            except Exception as e:
+                print(f'{e}')
+                continue
+
+            if (all([name, url, mileage, price, color, year])):
+                car['site'] = 'autoru'
+                car['name'] = name
+                car['url'] = url
+                car['image_src'] = 'No'
+                car['milage'] = mileage
+                car['price'] = price
+                car['color'] = color
+                car['year'] = year
+                result.append(car)
+
+        print("closing")
+        self.driver.close()
+        return result
+
+
+if __name__ == "__main__":
+    s = Session()
+    f = Car(brand="toyota", model="avensis")
+    p = AutoRuParser(f, s)
+    res = p.parse()
+    print(res)
